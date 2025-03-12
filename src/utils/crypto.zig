0404996770f0aec3004eb875fd3a3e05c7ef1892
Daniele Muffato <danixgithub1@gmail.com>
2025-03-12 18:32:04 +0100

    implemented encryption

--- src/utils/crypto.zig
+++ src/utils/crypto.zig
@@ -1,16 +1,20 @@
 const std = @import("std");
 
-const keySize: usize = 32;
-const blockSize: usize = 16;
+const key_size: usize = 32;
+const nonce_size: usize = 12;
+const tag_size: usize = 16;
 
-pub fn encrypt(allocator: std.mem.Allocator, key: [keySize]u8, message: []const u8) []const u8 {
+pub fn encrypt(allocator: std.mem.Allocator, key: [key_size]u8, message: []const u8) []const u8 {
+    _ = allocator;
+    const cipher: [message.len]u8 = undefined;
+    const tag: [tag_size]u8 = undefined;
+    const nonce: [nonce_size]u8 = undefined;
+    std.crypto.random.bytes(&nonce);
+    std.crypto.aead.aes_gcm.Aes256Gcm.encrypt(cipher, tag, message, "", nonce, key);
+}
+
+pub fn decrypt(allocator: std.mem.Allocator, key: [key_size]u8, message: []const u8) []const u8 {
     _ = allocator;
     _ = key;
     _ = message;
-}
-
-pub fn decrypt(allocator: std.mem.Allocator, key: [keySize]u8, message: []const u8) []const u8 {
-    std.debug.assert(message.len % blockSize == 0);
-    std.crypto.core.aes.Aes256.initDec(key).decryptWide(message.len / blockSize, "", "");
-    _ = allocator;
 }
