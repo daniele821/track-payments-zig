1c058f2332c404ee7428dd1b8374752264c62176
Daniele Muffato <danixgithub1@gmail.com>
2025-03-12 13:25:07 +0100

    costant u8 slice

--- src/utils/crypto.zig
+++ src/utils/crypto.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 
 const blockSize: usize = 32;
 
-pub fn encrypt(allocator: std.mem.Allocator, key: [blockSize]u8, message: []u8) []u8 {
+pub fn encrypt(allocator: std.mem.Allocator, key: [blockSize]u8, message: []const u8) []const u8 {
     _ = allocator;
     _ = key;
     _ = message;
 }
 
-pub fn decrypt(allocator: std.mem.Allocator, key: [blockSize]u8, message: []u8) []u8 {
+pub fn decrypt(allocator: std.mem.Allocator, key: [blockSize]u8, message: []const u8) []const u8 {
     _ = allocator;
     _ = key;
     _ = message;
