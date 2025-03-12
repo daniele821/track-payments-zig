a3de22c1c8d8ac4c139ea467ceb53c4f90a6a16b
Daniele Muffato <danixgithub1@gmail.com>
2025-03-12 18:54:09 +0100

    encryption should now be working!

--- src/utils/crypto.zig
+++ src/utils/crypto.zig
@@ -4,13 +4,22 @@ const key_size: usize = 32;
 const nonce_size: usize = 12;
 const tag_size: usize = 16;
 
-pub fn encrypt(allocator: std.mem.Allocator, key: [key_size]u8, message: []const u8) []const u8 {
-    _ = allocator;
-    const cipher: [message.len]u8 = undefined;
-    const tag: [tag_size]u8 = undefined;
-    const nonce: [nonce_size]u8 = undefined;
-    std.crypto.random.bytes(&nonce);
-    std.crypto.aead.aes_gcm.Aes256Gcm.encrypt(cipher, tag, message, "", nonce, key);
+pub fn encrypt(allocator: std.mem.Allocator, key: [key_size]u8, message: []const u8) ![]const u8 {
+    const result: []u8 = try allocator.alloc(u8, message.len + tag_size);
+    var tag: [tag_size]u8 = undefined;
+    var nonce: [nonce_size]u8 = undefined;
+    std.crypto.random.bytes(nonce[0..]);
+    std.crypto.aead.aes_gcm.Aes256Gcm.encrypt(result[0..message.len], &tag, message, "", nonce, key);
+    @memcpy(result[message.len..], tag[0..]);
+    return result;
+}
+
+test "AES256 encryption" {
+    const allocator = std.testing.allocator;
+    const key = [_]u8{'a'} ** key_size;
+    const msg = "b" ** 692;
+    const cipher = try encrypt(allocator, key, msg[0..]);
+    defer allocator.free(cipher);
 }
 
 pub fn decrypt(allocator: std.mem.Allocator, key: [key_size]u8, message: []const u8) []const u8 {
