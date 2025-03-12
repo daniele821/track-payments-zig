0fb20a5d898f948f974b138d487f2318b3f62f7b
Daniele Muffato <danixgithub1@gmail.com>
2025-03-12 14:07:27 +0100

    wip : crypto

--- src/utils/crypto.zig
+++ src/utils/crypto.zig
@@ -1,15 +1,16 @@
 const std = @import("std");
 
-const blockSize: usize = 32;
+const keySize: usize = 32;
+const blockSize: usize = 16;
 
-pub fn encrypt(allocator: std.mem.Allocator, key: [blockSize]u8, message: []const u8) []const u8 {
+pub fn encrypt(allocator: std.mem.Allocator, key: [keySize]u8, message: []const u8) []const u8 {
     _ = allocator;
     _ = key;
     _ = message;
 }
 
-pub fn decrypt(allocator: std.mem.Allocator, key: [blockSize]u8, message: []const u8) []const u8 {
+pub fn decrypt(allocator: std.mem.Allocator, key: [keySize]u8, message: []const u8) []const u8 {
+    std.debug.assert(message.len % blockSize == 0);
+    std.crypto.core.aes.Aes256.initDec(key).decryptWide(message.len / blockSize, "", "");
     _ = allocator;
-    _ = key;
-    _ = message;
 }
