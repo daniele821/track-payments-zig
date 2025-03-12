d3034e21d09f3fd848c20abd988ebba854e288be
Daniele Muffato <danixgithub1@gmail.com>
2025-03-12 19:08:26 +0100

    wip

--- src/utils/crypto.zig
+++ src/utils/crypto.zig
@@ -5,15 +5,15 @@ const nonce_size: usize = 12;
 const tag_size: usize = 16;
 
 pub fn encrypt(allocator: std.mem.Allocator, key: [key_size]u8, message: []const u8) ![]const u8 {
-    const result: []u8 = try allocator.alloc(u8, message.len + tag_size);
+    const cipher_tag: []u8 = try allocator.alloc(u8, message.len + tag_size);
     var tag: [tag_size]u8 = undefined;
     var nonce: [nonce_size]u8 = undefined;
     std.crypto.random.bytes(nonce[0..]);
 
-    std.crypto.aead.aes_gcm.Aes256Gcm.encrypt(result[0..message.len], &tag, message, "", nonce, key);
+    std.crypto.aead.aes_gcm.Aes256Gcm.encrypt(cipher_tag[0..message.len], &tag, message, "", nonce, key);
 
-    @memcpy(result[message.len..], tag[0..]);
-    return result;
+    @memcpy(cipher_tag[message.len..], tag[0..]);
+    return cipher_tag;
 }
 
 test "AES256 encryption" {
@@ -24,8 +24,12 @@ test "AES256 encryption" {
     defer allocator.free(cipher);
 }
 
-pub fn decrypt(allocator: std.mem.Allocator, key: [key_size]u8, message: []const u8) []const u8 {
-    _ = allocator;
-    _ = key;
-    _ = message;
-}
+// pub fn decrypt(allocator: std.mem.Allocator, key: [key_size]u8, cipher: []const u8) []const u8 {
+//     _ = allocator;
+//     _ = key;
+//     _ = cipher;
+//
+//     // std.crypto.aead.aes_gcm.Aes256Gcm.decrypt(m: []u8, c: []const u8, tag: [tag_length]u8, ad: []const u8, npub: [nonce_length]u8, key: [key_length]u8);
+//
+//     return message;
+// }
