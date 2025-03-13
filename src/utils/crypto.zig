78d096a4f01abcf6279438c8d72d976cdb4cc8ec
Daniele Muffato <danixgithub1@gmail.com>
2025-03-13 16:32:32 +0100

    return slices instead. this way if i every modify encryption, it wont break anywhere else

--- src/utils/crypto.zig
+++ src/utils/crypto.zig
@@ -3,17 +3,15 @@ const std = @import("std");
 pub const key_size: usize = 32;
 pub const nonce_size: usize = 12;
 pub const tag_size: usize = 16;
-pub const extra_size: usize = tag_size + nonce_size;
 
-pub fn encrypt(key: [key_size]u8, message: []const u8, cipher: []u8) void {
-    std.debug.assert(cipher.len == message.len + extra_size);
-
+pub fn encrypt(allocator: std.mem.Allocator, key: [key_size]u8, message: []const u8) ![]const u8 {
+    const cipher_tag = try allocator.alloc(u8, message.len + tag_size + nonce_size);
     var tag: [tag_size]u8 = undefined;
     var nonce: [nonce_size]u8 = undefined;
     std.crypto.random.bytes(nonce[0..]);
 
     std.crypto.aead.aes_gcm.Aes256Gcm.encrypt(
-        cipher[0..message.len],
+        cipher_tag[0..message.len],
         &tag,
         message,
         "",
@@ -21,29 +19,30 @@ pub fn encrypt(key: [key_size]u8, message: []const u8, cipher: []u8) void {
         key,
     );
 
-    @memcpy(cipher[message.len .. message.len + tag_size], tag[0..]);
-    @memcpy(cipher[message.len + tag_size ..], nonce[0..]);
+    @memcpy(cipher_tag[message.len .. message.len + tag_size], tag[0..]);
+    @memcpy(cipher_tag[message.len + tag_size ..], nonce[0..]);
+    return cipher_tag;
 }
 
 test "AES256 encryption" {
     const allocator = std.testing.allocator;
+    const msg_len = 32;
     const key = [_]u8{'a'} ** key_size;
-    const msg = "abcdefghjkilmnopqrstuvwxyz0123456789";
+    const msg = "b" ** msg_len;
 
-    const cipher = try allocator.alloc(u8, msg.len + extra_size);
+    // try encrypting
+    const cipher = try encrypt(allocator, key, msg[0..]);
     defer allocator.free(cipher);
-
-    encrypt(key, msg[0..], cipher);
 }
 
-pub fn decrypt(key: [key_size]u8, cipher: []const u8, message: []u8) !void {
-    std.debug.assert(message.len == cipher.len - extra_size);
-
-    const cipher_only = cipher[0..message.len];
+pub fn decrypt(allocator: std.mem.Allocator, key: [key_size]u8, cipher: []const u8) ![]const u8 {
+    const msg_len = cipher.len - tag_size - nonce_size;
+    const message = try allocator.alloc(u8, msg_len);
+    const cipher_only = cipher[0..msg_len];
     var tag: [tag_size]u8 = undefined;
     var nonce: [nonce_size]u8 = undefined;
-    @memcpy(tag[0..], cipher[message.len .. message.len + tag_size]);
-    @memcpy(nonce[0..], cipher[message.len + tag_size ..]);
+    @memcpy(tag[0..], cipher[msg_len .. msg_len + tag_size]);
+    @memcpy(nonce[0..], cipher[msg_len + tag_size ..]);
 
     try std.crypto.aead.aes_gcm.Aes256Gcm.decrypt(
         message,
@@ -53,19 +52,23 @@ pub fn decrypt(key: [key_size]u8, cipher: []const u8, message: []u8) !void {
         nonce,
         key,
     );
+
+    return message;
 }
 
 test "AES256 decryption" {
     const allocator = std.testing.allocator;
+    const msg_len = 32;
     const key = [_]u8{'a'} ** key_size;
-    const msg = "abcdefghjkilmnopqrstuvwxyz0123456789";
+    const msg = "b" ** msg_len;
 
-    const cipher = try allocator.alloc(u8, msg.len + extra_size);
+    // try encrypting
+    const cipher = try encrypt(allocator, key, msg[0..]);
     defer allocator.free(cipher);
-    const msg2 = try allocator.alloc(u8, msg.len);
+
+    // try decrypting
+    const msg2 = try decrypt(allocator, key, cipher);
     defer allocator.free(msg2);
 
-    encrypt(key, msg[0..], cipher);
-    try decrypt(key, cipher, msg2);
     try std.testing.expectEqualSlices(u8, msg, msg2);
 }
