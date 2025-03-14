1486c5daae7437241e5450912400b302aca8e30d
Daniele Muffato <danixgithub1@gmail.com>
2025-03-14 20:28:03 +0100

    AllPayments struct

--- src/payments/data.zig
+++ src/payments/data.zig
@@ -1,40 +1,47 @@
 const std = @import("std");
 
-const StringSet = std.StringHashMap(void);
+// const StringSet = std.StringHashMap(void);
 
-const ValueSet = struct {
-    cities: *StringSet,
-    shops: *StringSet,
-    methods: *StringSet,
-    items: *StringSet,
-
-    allPayments: *AllPayments,
-
-    pub fn init(allPayments: *AllPayments) ValueSet {
-        return .{
-            .allPayments = allPayments,
-            .cities = &StringSet.init(allPayments.allocator),
-        };
-    }
-
-    pub fn deinit() void {}
-};
-
-const Order = struct {
-    quantity: u32,
-    unit_price: u32,
-    item: []u8,
-};
+// const ValueSet = struct {
+//     allPayments: *AllPayments,
+//     cities: *StringSet,
+//     shops: *StringSet,
+//     methods: *StringSet,
+//     items: *StringSet,
+//
+//     const Self = @This();
+//
+//     pub fn init(allPayments: *AllPayments) Self {
+//         var cities = StringSet.init(allPayments.allocator);
+//         var shop = StringSet.init(allPayments.allocator);
+//         var methods = StringSet.init(allPayments.allocator);
+//         var items = StringSet.init(allPayments.allocator);
+//         return .{
+//             .allPayments = allPayments,
+//             .cities = &cities,
+//             .shops = &shop,
+//             .methods = &methods,
+//             .items = &items,
+//         };
+//     }
+//     pub fn deinit(self: *Self) void {
+//         self.cities.deinit();
+//         self.shops.deinit();
+//         self.methods.deinit();
+//         self.items.deinit();
+//     }
+// };
 
 const AllPayments = struct {
     allocator: std.mem.Allocator,
 
+    const Self = @This();
+
     pub fn init(allocator: std.mem.Allocator) AllPayments {
-        return .{
+        return Self{
             .allocator = allocator,
         };
     }
-
     pub fn deinit(self: *AllPayments) void {
         _ = self;
     }
