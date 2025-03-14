442c66cb2b99c216f744adc76329eb8f55eaaeb9
Daniele Muffato <danixgithub1@gmail.com>
2025-03-14 17:17:32 +0100

    new files

--- src/tests.zig
+++ src/tests.zig
@@ -3,4 +3,6 @@ const std = @import("std");
 test {
     _ = @import("./main.zig");
     _ = @import("./utils/crypto.zig");
+    _ = @import("./payments/data.zig");
+    _ = @import("./payments/errors.zig");
 }
