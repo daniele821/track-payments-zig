df77eac27afcf55e9d484139146f088b45a76163
Daniele Muffato <danixgithub1@gmail.com>
2025-03-14 20:43:37 +0100

    added ValueSet

--- src/payments/data.zig
+++ src/payments/data.zig
@@ -1,49 +1,45 @@
 const std = @import("std");
 
-// const StringSet = std.StringHashMap(void);
+const StringSet = std.StringHashMap(void);
 
-// const ValueSet = struct {
-//     allPayments: *AllPayments,
-//     cities: *StringSet,
-//     shops: *StringSet,
-//     methods: *StringSet,
-//     items: *StringSet,
-//
-//     const Self = @This();
-//
-//     pub fn init(allPayments: *AllPayments) Self {
-//         var cities = StringSet.init(allPayments.allocator);
-//         var shop = StringSet.init(allPayments.allocator);
-//         var methods = StringSet.init(allPayments.allocator);
-//         var items = StringSet.init(allPayments.allocator);
-//         return .{
-//             .allPayments = allPayments,
-//             .cities = &cities,
-//             .shops = &shop,
-//             .methods = &methods,
-//             .items = &items,
-//         };
-//     }
-//     pub fn deinit(self: *Self) void {
-//         self.cities.deinit();
-//         self.shops.deinit();
-//         self.methods.deinit();
-//         self.items.deinit();
-//     }
-// };
+const ValueSet = struct {
+    allocator: std.mem.Allocator,
+    cities: StringSet,
+    shops: StringSet,
+    methods: StringSet,
+    items: StringSet,
+
+    pub fn init(allocator: std.mem.Allocator) ValueSet {
+        return .{
+            .allocator = allocator,
+            .cities = StringSet.init(allocator),
+            .shops = StringSet.init(allocator),
+            .methods = StringSet.init(allocator),
+            .items = StringSet.init(allocator),
+        };
+    }
+    pub fn deinit(self: *ValueSet) void {
+        self.cities.deinit();
+        self.shops.deinit();
+        self.methods.deinit();
+        self.items.deinit();
+    }
+};
 
 const AllPayments = struct {
     allocator: std.mem.Allocator,
+    value_set: ValueSet,
 
     const Self = @This();
 
     pub fn init(allocator: std.mem.Allocator) AllPayments {
         return Self{
             .allocator = allocator,
+            .value_set = ValueSet.init(allocator),
         };
     }
-    pub fn deinit(self: *AllPayments) void {
-        _ = self;
+    pub fn deinit(self: *Self) void {
+        self.value_set.deinit();
     }
 };
 
