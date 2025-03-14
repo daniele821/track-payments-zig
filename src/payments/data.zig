d25685088e8b03f6b36b44770835119bb4698b08
Daniele Muffato <danixgithub1@gmail.com>
2025-03-14 19:58:45 +0100

    writing AllPayment struct

--- src/payments/data.zig
+++ src/payments/data.zig
@@ -1,1 +1,47 @@
 const std = @import("std");
+
+const StringSet = std.StringHashMap(void);
+
+const ValueSet = struct {
+    cities: *StringSet,
+    shops: *StringSet,
+    methods: *StringSet,
+    items: *StringSet,
+
+    allPayments: *AllPayments,
+
+    pub fn init(allPayments: *AllPayments) ValueSet {
+        return .{
+            .allPayments = allPayments,
+            .cities = &StringSet.init(allPayments.allocator),
+        };
+    }
+
+    pub fn deinit() void {}
+};
+
+const Order = struct {
+    quantity: u32,
+    unit_price: u32,
+    item: []u8,
+};
+
+const AllPayments = struct {
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) AllPayments {
+        return .{
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *AllPayments) void {
+        _ = self;
+    }
+};
+
+test "init" {
+    const allocator = std.testing.allocator;
+    var allPayments = AllPayments.init(allocator);
+    defer _ = allPayments.deinit();
+}
