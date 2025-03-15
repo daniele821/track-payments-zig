5c89bfbc171e0498fd4b7fdbc7516607a44a37db
Daniele Muffato <danixgithub1@gmail.com>
2025-03-15 15:39:22 +0100

    wip

--- src/utils/time.zig
+++ src/utils/time.zig
@@ -11,8 +11,22 @@ const Time = struct {
 
     pub fn fromMillisecs(timestamp_millisecs: u64) Time {
         const millisecs = timestamp_millisecs % std.time.ms_per_s;
+        const epoch_seconds = std.time.epoch.EpochSeconds{ .secs = @intCast(millisecs / 1000) };
+        const epoch_day = epoch_seconds.getEpochDay();
+        const day_seconds = epoch_seconds.getDaySeconds();
+        const year_day = epoch_day.calculateYearDay();
+        const month = year_day.calculateMonthDay();
+        const hours = day_seconds.getHoursIntoDay();
+        const minutes = day_seconds.getMinutesIntoHour();
+        const seconds = day_seconds.getSecondsIntoMinute();
 
         return .{
+            .years = year_day.year,
+            .months = month.month.numeric(),
+            .days = month.day_index + 1,
+            .hours = hours,
+            .minutes = minutes,
+            .secs = seconds,
             .millisecs = @intCast(millisecs),
         };
     }
@@ -24,9 +38,9 @@ const Time = struct {
 
 test "time from millisecs" {
     const time0 = Time.fromMillisecs(0);
-    const time1 = Time.fromMillisecs(4);
-    const time2 = Time.fromMillisecs(1742048633345);
-    try std.testing.expectEqualDeep(time0, Time{});
-    try std.testing.expectEqualDeep(time1, Time{ .millisecs = 4 });
-    try std.testing.expectEqualDeep(time2, Time{ .years = 2025, .months = 3, .days = 15, .hours = 14, .minutes = 23, .secs = 53, .millisecs = 345 });
+    const time1 = Time.fromMillisecs(1742048633345);
+    std.debug.print("{}\n", .{time0});
+    std.debug.print("{}\n", .{time1});
+    try std.testing.expectEqualDeep(Time{ .years = 1970, .months = 1, .days = 1 }, time0);
+    try std.testing.expectEqualDeep(Time{ .years = 2025, .months = 3, .days = 15, .hours = 14, .minutes = 23, .secs = 53, .millisecs = 345 }, time1);
 }
