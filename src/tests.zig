a0d54bc202c1c891f70e77f68f58bbef25738a5a
Daniele Muffato <danixgithub1@gmail.com>
2025-03-15 20:27:37 +0100

    removed time.zig : use a 3rd party lib instead

--- src/tests.zig
+++ src/tests.zig
@@ -3,6 +3,5 @@ const std = @import("std");
 test {
     _ = @import("./main.zig");
     _ = @import("./utils/crypto.zig");
-    _ = @import("./utils/time.zig");
     _ = @import("./payments/data.zig");
 }
