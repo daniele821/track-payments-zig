1dfcbfda63fa867c0ba633dfbe57d589d5ea025d
Daniele Muffato <danixgithub1@gmail.com>
2025-03-16 21:28:31 +0100

    hide internal structs

--- src/payments/data.zig
+++ src/payments/data.zig
@@ -3,7 +3,7 @@ const errors = @import("./errors.zig");
 
 const InsertError = errors.InsertError;
 
-pub const ValueSet = struct {
+const ValueSet = struct {
     allocator: std.mem.Allocator,
     cities: std.StringHashMap(void),
     shops: std.StringHashMap(void),
@@ -38,7 +38,7 @@ test "ValueSet init" {
     try value_set.methods.put("Method1", {});
 }
 
-pub const Order = struct {
+const Order = struct {
     quantity: u32,
     unit_price: u32,
     item: []const u8,
@@ -64,7 +64,7 @@ test "Order init" {
     try std.testing.expectError(InsertError.NotInValueSet, failure);
 }
 
-pub const Payment = struct {
+const Payment = struct {
     allocator: std.mem.Allocator,
     city: []const u8,
     shop: []const u8,
