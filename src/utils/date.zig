a381c8e1cc6a16cd6b59667fd15c6b7484f5c0ef
Daniele Muffato <danixgithub1@gmail.com>
2025-03-17 11:19:32 +0100

    added date conversion

--- src/utils/date.zig
+++ src/utils/date.zig
@@ -18,6 +18,15 @@ pub const DateUtil = struct {
     pub fn now(self: *DateUtil) !zdt.Datetime {
         return zdt.Datetime.now(.{ .tz = &self.timezone });
     }
+
+    pub fn unixToDatetime(unix_time: i64) !zdt.Datetime {
+        return zdt.Datetime.fromUnix(unix_time, zdt.Duration.Resolution.second, null);
+    }
+
+    pub fn datetimeToUnix(date_time: zdt.Datetime) i64 {
+        const unix_time = zdt.Datetime.toUnix(&date_time, zdt.Duration.Resolution.second);
+        return @intCast(unix_time);
+    }
 };
 
 test "DateUtil init" {
@@ -25,4 +34,11 @@ test "DateUtil init" {
     var date_unit = try DateUtil.init(allocator);
     defer date_unit.deinit();
     _ = try date_unit.now();
+}
+
+test "DateUtil conversions" {
+    const unix_time = 0;
+    const date_time = try DateUtil.unixToDatetime(unix_time);
+    const unix_time2 = DateUtil.datetimeToUnix(date_time);
+    try std.testing.expectEqual(unix_time, unix_time2);
 }
