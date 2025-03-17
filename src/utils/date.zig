7cdce22884538243331fbbd5c10ca6ab5ee3913d
Daniele Muffato <danixgithub1@gmail.com>
2025-03-17 10:37:12 +0100

    added DateUtil struct

--- src/utils/date.zig
+++ src/utils/date.zig
@@ -1,2 +1,23 @@
 const std = @import("std");
 const zdt = @import("zdt");
+
+pub const DateUtil = struct {
+    allocator: std.mem.Allocator,
+    timezone: zdt.Timezone,
+
+    pub fn init(allocator: std.mem.Allocator) !DateUtil {
+        return .{
+            .allocator = allocator,
+            .timezone = try zdt.Timezone.tzLocal(allocator),
+        };
+    }
+    pub fn deinit(self: *DateUtil) void {
+        self.timezone.deinit();
+    }
+};
+
+test "DateUtil init" {
+    const allocator = std.testing.allocator;
+    var date_unit = try DateUtil.init(allocator);
+    defer date_unit.deinit();
+}
