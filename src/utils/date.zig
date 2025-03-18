bc6cbf2b3b23f90475890e97100abcc4cce761c1
Daniele Muffato <danixgithub1@gmail.com>
2025-03-18 04:31:38 +0100

    added function to calculate current time, removing tz, without modifying the time

--- src/utils/date.zig
+++ src/utils/date.zig
@@ -14,10 +14,21 @@ pub const DateUtil = struct {
     pub fn deinit(self: *DateUtil) void {
         self.timezone.deinit();
     }
+
+    pub fn now(self: *DateUtil) !i64 {
+        const date = try zdt.Datetime.now(.{ .tz = &self.timezone });
+        var fields = date.toFields();
+        fields.tz_options = null;
+        fields.nanosecond = 0;
+        const localDate = try zdt.Datetime.fromFields(fields);
+        return @intCast(localDate.toUnix(zdt.Duration.Resolution.second));
+    }
 };
 
 test "DateUtil init" {
     const allocator = std.testing.allocator;
     var date_unit = try DateUtil.init(allocator);
     defer date_unit.deinit();
+
+    _ = try date_unit.now();
 }
