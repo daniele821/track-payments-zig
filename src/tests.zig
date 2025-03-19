f76f4f363a0027da48c51b507f6401a5a18d7e36
Daniele Muffato <danixgithub1@gmail.com>
2025-03-19 19:50:00 +0100

    renamed file

--- src/tests.zig
+++ src/tests.zig
@@ -4,6 +4,6 @@ test {
     _ = @import("./main.zig");
     _ = @import("./internal/crypto.zig");
     _ = @import("./internal/date.zig");
-    _ = @import("./internal/data.zig");
+    _ = @import("./internal/payments.zig");
     _ = @import("./internal/errors.zig");
 }
