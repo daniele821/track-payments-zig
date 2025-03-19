92886963197ccc628fed0bf7eb10a13e96034da1
Daniele Muffato <danixgithub1@gmail.com>
2025-03-19 19:48:52 +0100

    restructure

--- src/tests.zig
+++ src/tests.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 
 test {
     _ = @import("./main.zig");
-    _ = @import("./utils/crypto.zig");
-    _ = @import("./utils/date.zig");
-    _ = @import("./payments/data.zig");
-    _ = @import("./payments/errors.zig");
+    _ = @import("./internal/crypto.zig");
+    _ = @import("./internal/date.zig");
+    _ = @import("./internal/data.zig");
+    _ = @import("./internal/errors.zig");
 }
