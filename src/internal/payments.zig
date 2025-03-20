87a163be6b2eb11a5f322d9e20722cfcb8b50aa7
Daniele Muffato <danixgithub1@gmail.com>
2025-03-20 13:47:59 +0100

    moved into struct

--- src/internal/payments.zig
+++ src/internal/payments.zig
@@ -1,17 +1,17 @@
 const std = @import("std");
 
-const Elements = enum { items, cities, shops, methods };
-
-const Vtable = struct {
-    addElement: *const fn (ptr: *anyopaque, new_element: []const u8, elem_type: Elements) anyerror!void,
-    hasElement: *const fn (ptr: *anyopaque, element: []const u8, elem_type: Elements) bool,
-};
-
 pub const AllPayments = struct {
     ptr: *anyopaque,
     vtable: *const Vtable,
 
-    pub const Self = @This();
+    const Self = @This();
+
+    const Elements = enum { items, cities, shops, methods };
+
+    const Vtable = struct {
+        addElement: *const fn (ptr: *anyopaque, new_element: []const u8, elem_type: Elements) anyerror!void,
+        hasElement: *const fn (ptr: *anyopaque, element: []const u8, elem_type: Elements) bool,
+    };
 
     pub fn addElement(self: *Self, new_element: []const u8, elem_type: Elements) !void {
         return self.vtable.addElement(self, new_element, elem_type);
