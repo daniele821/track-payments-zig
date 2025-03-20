f154b4fa25a00dcceea7462f2bdb93a6071b79c7
Daniele Muffato <danixgithub1@gmail.com>
2025-03-20 17:58:37 +0100

    removed Self const

--- src/internal/payments.zig
+++ src/internal/payments.zig
@@ -7,18 +7,16 @@ pub const AllPayments = struct {
     ptr: *anyopaque,
     vtable: *const Vtable,
 
-    const Self = @This();
-
     const Vtable = struct {
         addElement: *const fn (ptr: *anyopaque, new_element: []const u8, elem_type: Elements) anyerror!void,
         hasElement: *const fn (ptr: *anyopaque, element: []const u8, elem_type: Elements) bool,
     };
 
-    pub fn addElement(self: *Self, new_element: []const u8, elem_type: Elements) !void {
+    pub fn addElement(self: *anyopaque, new_element: []const u8, elem_type: Elements) !void {
         return self.vtable.addElement(self, new_element, elem_type);
     }
 
-    pub fn hasElement(self: *Self, element: []const u8, elem_type: Elements) bool {
+    pub fn hasElement(self: *anyopaque, element: []const u8, elem_type: Elements) bool {
         return self.vtable.hasElement(self, element, elem_type);
     }
 };
