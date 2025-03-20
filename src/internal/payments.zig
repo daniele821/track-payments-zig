a177c2a960303f9b0f51beb101450b82b4189ccc
Daniele Muffato <danixgithub1@gmail.com>
2025-03-20 09:09:24 +0100

    building api

--- src/internal/payments.zig
+++ src/internal/payments.zig
@@ -1,1 +1,12 @@
 const std = @import("std");
+
+const Elements = enum { items, cities, shops, methods };
+
+const Vtable = struct {
+    addElement: *const fn (ptr: *anyopaque, new_element: []const u8, type: Elements) void,
+};
+
+pub const AllPayments = struct {
+    ptr: *anyopaque,
+    vtable: *const Vtable,
+};
