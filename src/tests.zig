a43f867e56012934a33363278cdd44b5ba7de9fc
Daniele Muffato <danixgithub1@gmail.com>
2025-03-22 23:07:35 +0100

    delete interface: just not use interfaces, they are a useless abstraction!

--- src/tests.zig
+++ src/tests.zig
@@ -4,7 +4,4 @@ test {
     _ = @import("./main.zig");
     _ = @import("./internal/utils/crypto.zig");
     _ = @import("./internal/utils/date.zig");
-    _ = @import("./internal/data/payments.zig");
-    _ = @import("./internal/data/payments_basic.zig");
-    _ = @import("./internal/data/payments_optimized.zig");
 }
