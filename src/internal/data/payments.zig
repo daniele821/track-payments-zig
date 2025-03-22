50c59b97df3ad36862d8af658027b8eeface9710
Daniele Muffato <danixgithub1@gmail.com>
2025-03-23 00:44:28 +0100

    efficient data structures inside Payments: store all data in arraylists, and then just reference them by index

--- src/internal/data/payments.zig
+++ src/internal/data/payments.zig
@@ -2,8 +2,25 @@ const std = @import("std");
 
 pub const Payments = struct {
     allocator: std.mem.Allocator,
+    strings_pool: std.ArrayList([]const u8),
+    payments_pool: std.ArrayList(Payment),
+    orders_pool: std.ArrayList(std.ArrayList(Order)),
 
     const Self = @This();
+
+    const Order = struct {
+        unit_price: u32,
+        quantity: u32,
+        item: u32,
+    };
+
+    const Payment = struct {
+        city: u32,
+        shop: u32,
+        method: u32,
+        date: i64,
+        orders: u32,
+    };
 
     pub fn init(allocator: std.mem.Allocator) Self {
         return Self{
