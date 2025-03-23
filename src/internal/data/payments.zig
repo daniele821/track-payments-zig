a614c420224ba7ee6343a3dd674b4e5197b18500
Daniele Muffato <danixgithub1@gmail.com>
2025-03-23 02:46:45 +0100

    new methods

--- src/internal/data/payments.zig
+++ src/internal/data/payments.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub const Elements = enum { city, shop, method, item };
+pub const ElementType = enum { city, shop, method, item };
 
 pub const Payments = struct {
     allocator: std.mem.Allocator,
@@ -10,7 +10,7 @@ pub const Payments = struct {
     const Self = @This();
     const StringPool = std.StringHashMapUnmanaged([]const u8);
     const ElementSet = std.AutoHashMapUnmanaged(*const []const u8, void);
-    const ElementSets = std.AutoHashMapUnmanaged(Elements, ElementSet);
+    const ElementSets = std.AutoHashMapUnmanaged(ElementType, ElementSet);
 
     const Order = struct {
         unit_price: u32,
@@ -42,10 +42,32 @@ pub const Payments = struct {
         }
         self.elements.deinit(self.allocator);
     }
+
+    pub fn addElement(self: *Self, new_element: []const u8, element_type: ElementType) !void {
+        _ = try self.strings_pool.getOrPut(self.allocator, new_element);
+        _ = element_type;
+    }
+
+    pub fn hasElement(self: *Self, element: []const u8, element_type: ElementType) bool {
+        _ = self;
+        _ = element;
+        _ = element_type;
+        return true;
+    }
 };
 
 test "payments" {
     const allocator = std.testing.allocator;
     var payments = Payments.init(allocator);
     defer payments.deinit();
+
+    try payments.addElement("Item", .item);
+    try payments.addElement("City", .city);
+    try payments.addElement("Method", .method);
+    try payments.addElement("Shop", .shop);
+
+    try std.testing.expect(payments.hasElement("Item", .item));
+    try std.testing.expect(payments.hasElement("City", .city));
+    try std.testing.expect(payments.hasElement("Method", .method));
+    try std.testing.expect(payments.hasElement("Shop", .shop));
 }
