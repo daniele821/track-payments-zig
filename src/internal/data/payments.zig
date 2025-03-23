67d4a91a194a583e0b887f120d1c041f8ab94828
Daniele Muffato <danixgithub1@gmail.com>
2025-03-23 01:28:06 +0100

    wip

--- src/internal/data/payments.zig
+++ src/internal/data/payments.zig
@@ -2,11 +2,19 @@ const std = @import("std");
 
 pub const Payments = struct {
     allocator: std.mem.Allocator,
-    strings_pool: std.ArrayList([]const u8),
-    payments_pool: std.ArrayList(Payment),
-    orders_pool: std.ArrayList(std.ArrayList(Order)),
+    strings_pool: StringPool,
+    payments_pool: PaymentsPool,
+    orders_pool: OrdersPool,
+    cities: StringHashMap,
+    shops: StringHashMap,
+    methods: StringHashMap,
+    items: StringHashMap,
 
     const Self = @This();
+    const StringPool = std.ArrayListUnmanaged([]const u8);
+    const PaymentsPool = std.ArrayListUnmanaged(Payment);
+    const OrdersPool = std.ArrayListUnmanaged(std.ArrayListUnmanaged(Order));
+    const StringHashMap = std.AutoHashMapUnmanaged(u32, void);
 
     const Order = struct {
         unit_price: u32,
@@ -25,14 +33,26 @@ pub const Payments = struct {
     pub fn init(allocator: std.mem.Allocator) Self {
         return Self{
             .allocator = allocator,
-            .strings_pool = std.ArrayList([]const u8).init(allocator),
-            .payments_pool = std.ArrayList(Payment).init(allocator),
-            .orders_pool = std.ArrayList(std.ArrayList(Order)).init(allocator),
+            .strings_pool = StringPool{},
+            .payments_pool = PaymentsPool{},
+            .orders_pool = OrdersPool{},
+            .cities = StringHashMap{},
+            .shops = StringHashMap{},
+            .methods = StringHashMap{},
+            .items = StringHashMap{},
         };
     }
 
     pub fn deinit(self: *Self) void {
-        _ = self;
+        // test i can nest arryylist
+        // if possible -> write loop to free OrdersPool inner lists
+        self.strings_pool.deinit(self.allocator);
+        self.payments_pool.deinit(self.allocator);
+        self.orders_pool.deinit(self.allocator);
+        self.cities.deinit(self.allocator);
+        self.shops.deinit(self.allocator);
+        self.methods.deinit(self.allocator);
+        self.items.deinit(self.allocator);
     }
 };
 
